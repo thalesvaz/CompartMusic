d4ef5485d4cf9c48a3c1ebe1b6465a9336c529a1
Thales Rangel Monteiro Vaz <thalesrmvaz@hotmail.com>
2016-05-13 00:54:43 -0300

    deletando classes p/ correção

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :playlist_musicas
-  resources :playlists
-  resources :musicas
-  resources :usuarios
   root to: 'visitors#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
